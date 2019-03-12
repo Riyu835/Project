0b237f4926d663a2219ac0953a3e1f2a323424f9
Ireka <azmi08305@gmail.cpm>
2019-03-12 14:11:14 +0700

    renew Scrolling Text and new CRUD example

---
+++ JavaAS/MyCRUD/app/src/main/java/com/example/mycdur/ListView.java
@@ -0,0 +1,13 @@
+package com.example.mycdur;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_item);
+    }
+}
