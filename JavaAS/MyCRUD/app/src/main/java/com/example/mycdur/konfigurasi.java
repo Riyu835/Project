f458262e07ec81a073546fc78587514e423699de
Risa <azmi08305@gmail.cpm>
2019-03-13 11:49:05 +0700

    Update the IP Address

--- JavaAS/MyCRUD/app/src/main/java/com/example/mycdur/konfigurasi.java
+++ JavaAS/MyCRUD/app/src/main/java/com/example/mycdur/konfigurasi.java
@@ -1,11 +1,11 @@
 package com.example.mycdur;
 
 public class konfigurasi {
-    public static final String URL_ADD = "http://192.168.2.2/pegawai/tambahPgw.php";
-    public static final String URL_GET_ALL = "http://192.168.2.2/pegawai/tampilSemuaPgw.php";
-    public static final String URL_GET_EMP = "http://192.168.2.2/pegawai/tampilPgw.php?id=";
-    public static final String URL_UPDATE_EMP = "http://192.168.2.2/pegawai/updatePgw.php";
-    public static final String URL_DELETE_EMP = "http://192.168.2.2/pegawai/hapusPgw.php?id=";
+    public static final String URL_ADD = "http://192.168.4.15/pegawai/tambahPgw.php";
+    public static final String URL_GET_ALL = "http://192.168.4.15/pegawai/tampilSemuaPgw.php";
+    public static final String URL_GET_EMP = "http://192.168.4.15/pegawai/tampilPgw.php?id=";
+    public static final String URL_UPDATE_EMP = "http://192.168.4.15/pegawai/updatePgw.php";
+    public static final String URL_DELETE_EMP = "http://192.168.4.15/pegawai/hapusPgw.php?id=";
 
     public static final String KEY_EMP_ID = "id";
     public static final String KEY_EMP_NAMA = "name";
