ad15612c1a70511c430bf0364a6d61eef3a8dc40
Risa <azmi08305@gmail.cpm>
2019-03-20 18:11:44 +0700

    PhoneNumberSpinner

---
+++ JavaAS/PhoneNumberSpinner/app/src/main/java/com/example/phonenumberspinner/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.phonenumberspinner;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private String mSpinnerLabel = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.label_spinner);
+        if (spinner != null) {
+            spinner.setOnItemSelectedListener(this);
+        }
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.labels_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource
+                (android.R.layout.simple_spinner_dropdown_item);
+
+        if (spinner != null) {
+            spinner.setAdapter(adapter);
+        }
+
+    }
+
+    public void showText(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText_main);
+        if (editText != null) {
+            String showString = (editText.getText().toString() + " - " + mSpinnerLabel);
+            TextView phoneNumberResult = (TextView) findViewById(R.id.text_phonelabel);
+            if (phoneNumberResult != null) phoneNumberResult.setText(showString);
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
+        mSpinnerLabel = adapterView.getItemAtPosition(pos).toString();
+        showText(view);
+    }
+
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        Log.d(TAG, getString(R.string.nothing_selected));
+    }
+}
